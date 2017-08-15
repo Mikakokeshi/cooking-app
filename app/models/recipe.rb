6835eb8100b3e8f307bee703563c044f8bbd7416
Mikako Iijima <mikakokeshi00124@gmail.com>
2017-08-15 08:44:50 +0000

    add-pic

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,5 @@
 class Recipe < ActiveRecord::Base
   belongs_to :user
   validates :user_id, :title, :content, presence: true
+  mount_uploader :image, ImageUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170815044318) do
+ActiveRecord::Schema.define(version: 20170815082336) do
 
   create_table "recipes", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170815044318) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "image"
   end
 
   add_index "recipes", ["user_id"], name: "index_recipes_on_user_id"
