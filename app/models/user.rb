65d3ec1f67f2a77ec771fd349d1a1197039b5057
obikanubi <trevor.c.cruz@gmail.com>
2017-08-15 08:54:39 +0000

    done setting up follower and following ids

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,11 @@ class User < ActiveRecord::Base
   has_many :active_relationships, class_name: "Relationship",
                                   foreign_key: "follower_id",
                                   dependent: :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
   has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships
   
   mount_uploader :avatar, AvatarUploader
 
@@ -21,10 +25,19 @@ class User < ActiveRecord::Base
       user.password = Devise.friendly_token[0,20]
       #user.name = auth.info.name   # assuming the user model has a name
       #user.image = auth.info.image # assuming the user model has an image
-      # If you are using confirmable and the provider(s) you use validate emails, 
-      # uncomment the line below to skip the confirmation emails.
-      # user.skip_confirmation!
     end
+  end
+  
+  def follow(other_user)
+    following << other_user
+  end
+  
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+  
+  def following?(other_user)
+    following.include?(other_user)
   end
   
     private
